17975e35d0bccb77269cbaeb479b4a62f49721ee
Nick <darkbahamut@gmail.com>
2019-01-06 14:23:35 +0000

    Linux 4.9.68

--- drivers/gpu/arm/tHEx/r12p0/mali_kbase_sync_file.c
+++ drivers/gpu/arm/tHEx/r12p0/mali_kbase_sync_file.c
@@ -169,9 +169,7 @@ static void kbase_fence_wait_callback(struct dma_fence *fence,
 	struct kbase_context *kctx = katom->kctx;
 
 	/* Cancel atom if fence is erroneous */
-#if (KERNEL_VERSION(4, 11, 0) <= LINUX_VERSION_CODE || \
-	 (KERNEL_VERSION(4, 10, 0) > LINUX_VERSION_CODE && \
-	  KERNEL_VERSION(4, 9, 68) <= LINUX_VERSION_CODE))
+#if (KERNEL_VERSION(4, 10, 0) <= LINUX_VERSION_CODE)
 	if (dma_fence_is_signaled(kcb->fence) && kcb->fence->error)
 #else
 	if (dma_fence_is_signaled(kcb->fence) && kcb->fence->status < 0)
@@ -287,9 +285,7 @@ static void kbase_sync_fence_info_get(struct dma_fence *fence,
 	 * 1 : signaled
 	 */
 	if (dma_fence_is_signaled(fence)) {
-#if (KERNEL_VERSION(4, 11, 0) <= LINUX_VERSION_CODE || \
-	 (KERNEL_VERSION(4, 10, 0) > LINUX_VERSION_CODE && \
-	  KERNEL_VERSION(4, 9, 68) <= LINUX_VERSION_CODE))
+#if (KERNEL_VERSION(4, 10, 0) <= LINUX_VERSION_CODE)
 		int status = fence->error;
 #else
 		int status = fence->status;
